2c1e34e3a3badfde51711684eae31efed4971e28
23MADDY <96527975+23MADDY@users.noreply.github.com>
2022-05-15 00:20:15 +0545

    Update index.js

--- weather map/index.js
+++ weather map/index.js
@@ -1,14 +1,4 @@
-//const NewApi ='ebdeb6e2f96448af00886a1f95c65b5f'
-//const url =  (location) => `https:api.openweathermap.org/data/2.5/weather?q=${location}&appid=${NewApi}`
 
-//async function getWeeatherByLoc(location) {
-//const resp = await fetch(url(location), {origin: "cors"});
-//const respData = await resp.json();
-
-//console.log(respData);
-//}
-
-//getWeeatherByLoc("Nepal")
 const input = document.querySelector("#input")
 const btn = document.querySelector("#btn")
 const I =  document.querySelector(".IN")
